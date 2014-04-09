29acfa1257c70d672f0a4af541ca96d111442c9e
Alumno <Alumno@EQ16-LAB1>
2014-04-09 19:37:36 -0300

    agregamos ventanita wey

---
+++ lab03/src/lab03/Ventana.java
@@ -0,0 +1,12 @@
+package lab03;
+import javax.swing.JFrame; 
+
+public class Ventana {
+	public static void main(String[] args){ 
+	    JFrame frame = new JFrame(); 
+	    frame.setTitle("ola k ase"); 
+	    frame.setSize(800,600); 
+	    //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	    frame.setVisible(true); 
+	}
+}
